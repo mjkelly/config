b9592a61e846d25586835c358fb4646e2b188760
Michael Kelly <michael@michaelkelly.org>
2013-04-13 13:48:43 -0400

    Make battery display brighter.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -80,7 +80,7 @@ if battery_file then
   mybattwidget.width = 32
   mybattwidget.background_color = "#222222"
   vicious.register(mybattwidget, vicious.widgets.bat,
-      "<span color='#7777BB' size='large'>$1</span><span color='#7777BB'>$2</span>", 61, battery)
+      "<span color='#1BDDE0' size='large'>$1</span><span color='#1BDDE0'>$2</span>", 61, battery)
 end
 
 mynetwidget_up = widget({ type = "textbox" })
