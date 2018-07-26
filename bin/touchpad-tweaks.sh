8e6f04b22ff5a8b609a10acb18be71e09a0ddcdd
Michael Kelly <michael@michaelkelly.org>
2018-07-25 22:25:48 -0400

    Add openbox touchpad setup on login

---
+++ bin/touchpad-tweaks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+logger "Running $0"
+
+/usr/bin/synclient MaxTapTime=180 TapButton1=1 \
+  PalmDetect=1 PalmMinWidth=5 PalmMinZ=40 \
+  VertEdgeScroll=0 ClickFinger2=3 \
+  HorizHysteresis=50 VertHysteresis=50 \
+  CoastingSpeed=0 \
+  RightButtonAreaLeft=0 RightButtonAreaRight=0 \
+  RightButtonAreaTop=0 RightButtonAreaBottom=0 \
+  VertScrollDelta=-111 HorizScrollDelta=-111
+
